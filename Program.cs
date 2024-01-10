8e75118a9c86ccaf1a9524e85180457679acc925
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 10:23:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22__Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs song = new Songs("Holiday","vision",4);
+            Songs song2 = new Songs("Broken", "vision", 4);
+
+            Console.WriteLine(Songs.songsCount);
+        }
+    }
+}
---
+++ Songs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22__Static_Attribute
+{
+    internal class Songs
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+        public static int songsCount =0;
+        public Songs(string title, string artist, int duration)
+        {
+            this.title = title;
+            this.artist = artist;
+            this.duration = duration;
+            songsCount++;
+        }
+    }
+}
